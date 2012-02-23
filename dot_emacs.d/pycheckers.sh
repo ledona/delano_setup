1a58fc448437418573c89219e63025d906e19044
Delano J. McFarlane <delano@ledona.net>
2012-02-23 13:00:14 -0500

    removed some pep8 formating errors from pycheckers

--- dot_emacs.d/pycheckers.sh
+++ dot_emacs.d/pycheckers.sh
@@ -2,5 +2,5 @@
 
 # If this does not work then the progs are likely not in the path that is recogned by emac
 pyflakes "$1"
-pep8 --ignore=E221,E701,E202 --repeat "$1"
+pep8 --ignore=W293,E501,E221,E701,E202,W291 --repeat "$1"
 true
