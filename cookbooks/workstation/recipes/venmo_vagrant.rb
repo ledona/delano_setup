393dcb7de93d12e81ee93cb67aadb588cd72ac6b
ledona <delano@ledona.net>
2014-07-06 02:23:37 -0400

    house keeping

--- cookbooks/workstation/recipes/venmo_vagrant.rb
+++
@@ -1,8 +0,0 @@
-dotfiles_path = File.expand_path(File.dirname(__FILE__)+"../../../..") # a few levels up from this file
-
-# make sure that my additional shell setup is run
-execute "Add dot_bash_profile to .bash_profile" do
-  command "echo '
-source #{dotfiles_path}/dot_bash_venmo_vagrant.sh' >> '#{ENV['HOME']}/.bash_profile'"
-  not_if "grep '#{dotfiles_path}/dot_bash_venmo_vagrant_profile' '#{ENV['HOME']}/.bash_profile'"
-end
